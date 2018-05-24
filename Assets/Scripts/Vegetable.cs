cbb603af062bc7df48ae623c77a4f7a6c819986e
gamedev1991 <39465084+gamedev1991@users.noreply.github.com>
2018-05-25 03:22:14 +0530

    Made controls for 2nd player
    Added trigger zones for picking up of vegetables
    Added controls for player2 
    Implemented following of hud with players

---
+++ Assets/Scripts/Vegetable.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vegetable : MonoBehaviour {
+	public ThirdPersonUserControl thirdPersonUserControl;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseDown()
+	{
+		if (thirdPersonUserControl.isInsideVegetablePickUpZone) {
+			string objectName = (this.gameObject.name.Remove (0, this.gameObject.name.Length - 1));
+			char charToBeAdded = objectName [0];
+			if (thirdPersonUserControl.rawVegetables.Count <= 2) {
+				if (!thirdPersonUserControl.rawVegetables.Contains (charToBeAdded)) {
+					thirdPersonUserControl.rawVegetables.Add (charToBeAdded);
+				} else {
+					thirdPersonUserControl.rawVegetables.Remove (charToBeAdded);
+				}
+				if (thirdPersonUserControl.rawVegetables.Count == 0) {
+					thirdPersonUserControl.rawVegetableHUD.SetActive (false);
+				} else {
+					thirdPersonUserControl.rawVegetableHUD.SetActive (true);
+					thirdPersonUserControl.SetHUD ();
+				}
+			} 
+		}
+	}
+}
